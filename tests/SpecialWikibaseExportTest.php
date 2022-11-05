1773806ee10002b1acea65b0983b9d21f64d357c
Morne Alberts <morne@malberts.net>
2022-11-05 11:21:09 +0200

    Add stub special page

---
+++ tests/SpecialWikibaseExportTest.php
@@ -0,0 +1,28 @@
+<?php
+
+declare( strict_types = 1 );
+
+namespace ProfessionalWiki\WikibaseExport\Tests;
+
+use ProfessionalWiki\WikibaseExport\SpecialWikibaseExport;
+use SpecialPageTestBase;
+
+/**
+ * @covers \ProfessionalWiki\WikibaseExport\SpecialWikibaseExport
+ */
+class SpecialWikibaseExportTest extends SpecialPageTestBase {
+
+	protected function newSpecialPage(): SpecialWikibaseExport {
+		return new SpecialWikibaseExport();
+	}
+
+	public function testStub(): void {
+		list( $output ) = $this->executeSpecialPage();
+
+		$this->assertStringContainsString(
+			'TODO',
+			$output
+		);
+	}
+
+}
