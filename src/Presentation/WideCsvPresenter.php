87dbe7b3cace3811cbce950293b0a0f8fb14d7e5
Jeroen De Dauw <jeroendedauw@gmail.com>
2022-11-19 17:25:05 +0100

    Remove ExportResponse

--- src/Presentation/WideCsvPresenter.php
+++ src/Presentation/WideCsvPresenter.php
@@ -5,11 +5,11 @@ declare( strict_types = 1 );
 namespace ProfessionalWiki\WikibaseExport\Presentation;
 
 use ProfessionalWiki\WikibaseExport\Application\Export\ExportPresenter;
-use ProfessionalWiki\WikibaseExport\Application\Export\ExportResponse;
+use ProfessionalWiki\WikibaseExport\Application\Export\MappedEntity;
 
 class WideCsvPresenter implements ExportPresenter {
 
-	public function present( ExportResponse $response ): void {
+	public function present( MappedEntity $entity ): void {
 		// TODO
 	}
 
