8eafec89bd4b26455f2d9ea28d918e03daeb6f79
Morne Alberts <morne@malberts.net>
2022-11-23 10:48:00 +0200

    Add valid Config test double

--- tests/Application/Export/ExportUseCaseTest.php
+++ tests/Application/Export/ExportUseCaseTest.php
@@ -13,6 +13,7 @@ use ProfessionalWiki\WikibaseExport\Application\Export\ExportRequest;
 use ProfessionalWiki\WikibaseExport\Application\Export\ExportUseCase;
 use ProfessionalWiki\WikibaseExport\Tests\TestDoubles\SpyExportPresenter;
 use ProfessionalWiki\WikibaseExport\Tests\TestDoubles\StubConfigLookup;
+use ProfessionalWiki\WikibaseExport\Tests\TestDoubles\Valid;
 use ProfessionalWiki\WikibaseExport\WikibaseExportExtension;
 use Wikibase\Repo\WikibaseRepo;
 
@@ -21,7 +22,7 @@ use Wikibase\Repo\WikibaseRepo;
  */
 class ExportUseCaseTest extends TestCase {
 
-	public function newExportUseCase( Config $wikiConfig, ExportPresenter $presenter ): ExportUseCase {
+	private function newExportUseCase( Config $wikiConfig, ExportPresenter $presenter ): ExportUseCase {
 		return new ExportUseCase(
 			entitySourceBuilder: new EntitySourceBuilder(
 				lookup: WikibaseRepo::getEntityLookup()
@@ -39,11 +40,7 @@ class ExportUseCaseTest extends TestCase {
 	public function testNothingIsPresented(): void {
 		$presenter = new SpyExportPresenter();
 		$useCase = $this->newExportUseCase(
-			new Config(
-				startTimePropertyId: 'P1',
-				endTimePropertyId: 'P2',
-				pointInTimePropertyId: 'P3'
-			),
+			Valid::validConfig(),
 			$presenter
 		);
 
@@ -64,7 +61,10 @@ class ExportUseCaseTest extends TestCase {
 
 	public function testInvalidRequestIsPresented(): void {
 		$presenter = new SpyExportPresenter();
-		$useCase = WikibaseExportExtension::getInstance()->newExportUseCase( $presenter );
+		$useCase = $this->newExportUseCase(
+			Valid::validConfig(),
+			$presenter
+		);
 
 		$useCase->export(
 			new ExportRequest(
