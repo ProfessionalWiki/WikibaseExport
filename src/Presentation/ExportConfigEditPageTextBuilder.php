a45ff50f1c085d1d45c5aee37b9c9830f108d758
Morne Alberts <morne@malberts.net>
2022-11-25 19:00:14 +0200

    Add translated strings and ensure everything is escaped

--- src/Presentation/ExportConfigEditPageTextBuilder.php
+++ src/Presentation/ExportConfigEditPageTextBuilder.php
@@ -5,6 +5,7 @@ declare( strict_types = 1 );
 namespace ProfessionalWiki\WikibaseExport\Presentation;
 
 use IContextSource;
+use Title;
 
 class ExportConfigEditPageTextBuilder {
 
@@ -14,56 +15,103 @@ class ExportConfigEditPageTextBuilder {
 	}
 
 	public function createHtml(): string {
-		$html = '<div class="wikibase-export-config-help">';
-
-		$html .= '<h2>' . $this->context->msg( 'wikibase-export-config-help' ) . '</h2>';
-		$html .= $this->creatVariablesSection();
-		$html .= $this->createExampleSection();
-		$html .= $this->createMessagesSection();
-
-		$html .= '</div>';
-
-		return $html;
+		return '<div class="wikibase-export-config-help">' .
+			'<h2>' . $this->context->msg( 'wikibase-export-config-help' )->escaped() . '</h2>' .
+			$this->creatVariablesSection() .
+			$this->createExampleSection() .
+			$this->createMessagesSection() .
+			'</div>';
 	}
 
 	private function creatVariablesSection(): string {
-		$html = '<h3>' . $this->context->msg( 'wikibase-export-config-help-variables' ) . '</h3>';
-		$html .= '
-<table class="wikitable">
-	<thead>
-		<tr><th>Variable</th><th>Description</th><th>Example</th></tr>
-	</thead>
-	<tbdoy>
-		<tr><td><code>defaultSubjects</code></td><td>A list of item IDs to be selected by default on the form.</td><td><code>[ "Q1", "Q2" ]</code></td></tr>
-		<tr><td><code>defaultStartYear</code></td><td>The default start year on the form.</td><td><code>2010</code></td></tr>
-		<tr><td><code>defaultEndYear</code></td><td>The default end year on the form.</td><td><code>2022</code></td></tr>
-		<tr><td><code>startTimePropertyId</code></td><td>Property ID of the qualifier used for the start of a time range.</td><td><code>"P100"</code></td></tr>
-		<tr><td><code>endTimePropertyId</code></td><td>Property ID of the qualifier used for the end of a time range.</td><td><code>"P200"</code></td></tr>
-		<tr><td><code>pointInTimePropertyId</code></td><td>Property ID of the qualifier used for a specific point in time.</td><td><code>"P300"</code></td></tr>
-		<tr><td><code>properties</code></td><td>A list of property IDs for statements that may be included in the export.</td><td><code>[ "P1", "P2" ]</code></td></tr>
-	</tbdoy>
-</table>
-';
+		return '<h3>' . $this->context->msg( 'wikibase-export-config-help-variables' )->escaped() . '</h3>' .
+			'<table class="wikitable">' .
+			'<thead><tr>' .
+			'<th>' . $this->context->msg( 'wikibase-export-config-help-table-variable' )->escaped() . '</th>' .
+			'<th>' . $this->context->msg( 'wikibase-export-config-help-table-description' )->escaped() . '</th>' .
+			'<th>' . $this->context->msg( 'wikibase-export-config-help-table-example' )->escaped() . '</th>' .
+			'</tr></thead>' .
+			'<tbody>' .
+			$this->createTableRow(
+				'defaultSubjects',
+				'wikibase-export-config-help-variable-default-subjects',
+				'[ "Q1", "Q2" ]'
+			) .
+			$this->createTableRow(
+				'defaultStartYear',
+				'wikibase-export-config-help-variable-default-start-year',
+				'2010'
+			) .
+			$this->createTableRow(
+				'defaultEndYear',
+				'wikibase-export-config-help-variable-default-end-year',
+				'2022'
+			) .
+			$this->createTableRow(
+				'startTimePropertyId',
+				'wikibase-export-config-help-variable-start-time-property-id',
+				'"P100"'
+			) .
+			$this->createTableRow(
+				'endTimePropertyId',
+				'wikibase-export-config-help-variable-end-time-property-id',
+				'"P200"'
+			) .
+			$this->createTableRow(
+				'pointInTimePropertyId',
+				'wikibase-export-config-help-variable-point-in-time-property-id',
+				'"P300"'
+			) .
+			$this->createTableRow(
+				'properties',
+				'wikibase-export-config-help-variable-properties',
+				'[ "P1", "P2" ]'
+			) .
+			'</tbody>' .
+			'</table>';
+	}
 
-		return $html;
+	private function createTableRow( string $variable, string $messagePart, string $example ): string {
+		return '<tr>' .
+			'<td><code>' . $variable . '</code></td>' .
+			'<td>' . $this->context->msg( "$messagePart" )->escaped() . '</td>' .
+			'<td><code>' . $example . '</code></td>' .
+			'</tr>';
 	}
 
 	private function createMessagesSection(): string {
-		$html = '<h3>' . $this->context->msg( 'wikibase-export-config-help-messages' ) . '</h3>';
-		$html .= '<ul>';
-		$html .= '<li>' . $this->context->msg( 'wikibase-export-config-help-intro' ) . '</li>';
-		$html .= '<li>' . $this->context->msg( 'wikibase-export-config-help-subjects-heading' ) . '</li>';
-		$html .= '<li>' . $this->context->msg( 'wikibase-export-config-help-subjects-placeholder' ) . '</li>';
-		$html .= '</ul>';
+		return '<h3>' . $this->context->msg( 'wikibase-export-config-help-messages' )->escaped() . '</h3>' .
+			'<ul>' .
+			$this->createMessageLinkItem(
+				'wikibase-export-intro',
+				'wikibase-export-config-help-message-intro'
+			) .
+			$this->createMessageLinkItem(
+				'wikibase-export-subjects-heading',
+				'wikibase-export-config-help-message-subjects-heading'
+			) .
+			$this->createMessageLinkItem(
+				'wikibase-export-subjects-placeholder',
+				'wikibase-export-config-help-message-subjects-placeholder'
+			) .
+			'</ul>';
+	}
 
-		return $html;
+	private function createMessageLinkItem( string $targetMessage, string $textMessage ): string {
+		$title = Title::newFromText( "MediaWiki:$targetMessage" );
+
+		if ( $title === null ) {
+			return '';
+		}
+
+		return '<li><a href="' . $title->getLocalURL() . '">' .
+			$this->context->msg( $textMessage )->escaped() .
+			'</a></li>';
 	}
 
 	private function createExampleSection(): string {
-		$html = '<h3>' . $this->context->msg( 'wikibase-export-config-help-example' ) . '</h3>';
-		$html .= '<pre>' . $this->getExampleContents() . '</pre>';
-
-		return $html;
+		return '<h3>' . $this->context->msg( 'wikibase-export-config-help-example' )->escaped() . '</h3>' .
+			'<pre>' . $this->getExampleContents() . '</pre>';
 	}
 
 	private function getExampleContents(): string {
