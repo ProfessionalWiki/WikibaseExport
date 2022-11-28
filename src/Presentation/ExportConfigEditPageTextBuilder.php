a7c9b2944c1d40b60134556453944495918ca435
Morne Alberts <morne@malberts.net>
2022-11-28 19:57:00 +0200

    Add required variables on config page

--- src/Presentation/ExportConfigEditPageTextBuilder.php
+++ src/Presentation/ExportConfigEditPageTextBuilder.php
@@ -42,54 +42,69 @@ class ExportConfigEditPageTextBuilder {
 			'<thead><tr>' .
 			'<th>' . $this->context->msg( 'wikibase-export-config-help-table-variable' )->escaped() . '</th>' .
 			'<th>' . $this->context->msg( 'wikibase-export-config-help-table-description' )->escaped() . '</th>' .
+			'<th>' . $this->context->msg( 'wikibase-export-config-help-table-required' )->escaped() . '</th>' .
 			'<th>' . $this->context->msg( 'wikibase-export-config-help-table-example' )->escaped() . '</th>' .
 			'</tr></thead>' .
 			'<tbody>' .
 			$this->createTableRow(
 				'defaultSubjects',
 				'wikibase-export-config-help-variable-default-subjects',
-				'[ "Q1", "Q2" ]'
+				'[ "Q1", "Q2" ]',
+				false
 			) .
 			$this->createTableRow(
 				'defaultStartYear',
 				'wikibase-export-config-help-variable-default-start-year',
-				'2010'
+				'2010',
+				false
 			) .
 			$this->createTableRow(
 				'defaultEndYear',
 				'wikibase-export-config-help-variable-default-end-year',
-				'2022'
+				'2022',
+				false
 			) .
 			$this->createTableRow(
 				'startTimePropertyId',
 				'wikibase-export-config-help-variable-start-time-property-id',
-				'"P100"'
+				'"P100"',
+				true
 			) .
 			$this->createTableRow(
 				'endTimePropertyId',
 				'wikibase-export-config-help-variable-end-time-property-id',
-				'"P200"'
+				'"P200"',
+				true
 			) .
 			$this->createTableRow(
 				'pointInTimePropertyId',
 				'wikibase-export-config-help-variable-point-in-time-property-id',
-				'"P300"'
+				'"P300"',
+				true
 			) .
 			$this->createTableRow(
 				'properties',
 				'wikibase-export-config-help-variable-properties',
-				'[ "P1", "P2" ]'
+				'[ "P1", "P2" ]',
+				true
 			) .
 			'</tbody>' .
 			'</table>';
 	}
 
-	private function createTableRow( string $variable, string $messagePart, string $example ): string {
+	private function createTableRow( string $variable, string $messagePart, string $example, bool $required ): string {
 		return '<tr>' .
 			'<td>' . Html::element( 'code', [], $variable ) . '</td>' .
-			'<td>' . $this->context->msg( "$messagePart" )->escaped() . '</td>' .
+			'<td>' . $this->context->msg( $messagePart )->escaped() . '</td>' .
+			'<td>' . $this->context->msg( $this->getRequiredMessage( $required ) )->escaped() . '</td>' .
 			'<td>' . Html::element( 'code', [], $example ) . '</td>' .
 			'</tr>';
+	}
+
+	private function getRequiredMessage( bool $required ): string {
+		return $required
+			? 'wikibase-export-config-help-table-required-yes'
+			: 'wikibase-export-config-help-table-required-no';
 	}
 
 	private function createMessagesSection(): string {
