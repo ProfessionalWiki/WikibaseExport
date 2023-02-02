d434fc6f5b970b9d2a22aa8717014907d2eebac1
Jeroen De Dauw <jeroendedauw@gmail.com>
2023-02-02 19:24:59 +0100

    Remove obsolete "required" column

--- src/Presentation/ExportConfigEditPageTextBuilder.php
+++ src/Presentation/ExportConfigEditPageTextBuilder.php
@@ -42,7 +42,6 @@ class ExportConfigEditPageTextBuilder {
 			'<thead><tr>' .
 			'<th>' . $this->context->msg( 'wikibase-export-config-help-table-variable' )->escaped() . '</th>' .
 			'<th>' . $this->context->msg( 'wikibase-export-config-help-table-description' )->escaped() . '</th>' .
-			'<th>' . $this->context->msg( 'wikibase-export-config-help-table-required' )->escaped() . '</th>' .
 			'<th>' . $this->context->msg( 'wikibase-export-config-help-table-example' )->escaped() . '</th>' .
 			'</tr></thead>' .
 			'<tbody>' .
@@ -50,85 +49,67 @@ class ExportConfigEditPageTextBuilder {
 				'startTimePropertyId',
 				'wikibase-export-config-help-variable-start-time-property-id',
 				'"P100"',
-				true
 			) .
 			$this->createTableRow(
 				'endTimePropertyId',
 				'wikibase-export-config-help-variable-end-time-property-id',
 				'"P200"',
-				true
 			) .
 			$this->createTableRow(
 				'pointInTimePropertyId',
 				'wikibase-export-config-help-variable-point-in-time-property-id',
 				'"P300"',
-				true
 			) .
 			$this->createTableRow(
 				'propertiesToGroupByYear',
 				'wikibase-export-config-help-variable-properties-with-qualifiers',
 				'[ "P1", "P2" ]',
-				false
 			) .
 			$this->createTableRow(
 				'ungroupedProperties',
 				'wikibase-export-config-help-variable-properties-without-qualifiers',
 				'[ "P3", "P4" ]',
-				false
 			) .
 			$this->createTableRow(
 				'defaultSubjects',
 				'wikibase-export-config-help-variable-default-subjects',
 				'[ "Q1", "Q2" ]',
-				false
 			) .
 			$this->createTableRow(
 				'defaultStartYear',
 				'wikibase-export-config-help-variable-default-start-year',
 				'2010',
-				false
 			) .
 			$this->createTableRow(
 				'defaultEndYear',
 				'wikibase-export-config-help-variable-default-end-year',
 				'2022',
-				false
 			) .
 			$this->createTableRow(
 				'subjectFilterPropertyId',
 				'wikibase-export-config-help-variable-subject-filter-property-id',
 				'"P50"',
-				false
 			) .
 			$this->createTableRow(
 				'subjectFilterPropertyValue',
 				'wikibase-export-config-help-variable-subject-filter-property-value',
 				'"company"',
-				false
 			) .
 			$this->createTableRow(
 				'exportLanguages',
 				'wikibase-export-config-help-variable-export-languages',
 				'[ "en", "nl" ]',
-				false
 			) .
 			'</tbody>' .
 			'</table>';
 	}
 
-	private function createTableRow( string $variable, string $messagePart, string $example, bool $required ): string {
+	private function createTableRow( string $variable, string $messagePart, string $example ): string {
 		return '<tr>' .
 			'<td>' . Html::element( 'code', [], $variable ) . '</td>' .
 			'<td>' . $this->context->msg( $messagePart )->escaped() . '</td>' .
-			'<td>' . $this->context->msg( $this->getRequiredMessage( $required ) )->escaped() . '</td>' .
 			'<td>' . Html::element( 'code', [], $example ) . '</td>' .
 			'</tr>';
-	}
-
-	private function getRequiredMessage( bool $required ): string {
-		return $required
-			? 'wikibase-export-config-help-table-required-yes'
-			: 'wikibase-export-config-help-table-required-no';
 	}
 
 	private function createMessagesSection(): string {
